df6726fd203f670e1ebc493a301e38709ed1f506
mahdic200 <rmahdi170@gmail.com>
2022-07-30 22:26:51 +0430

    end of episode85 middlwares on a specefic route

--- app/Http/Middleware/CheckAge.php
+++ app/Http/Middleware/CheckAge.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class CheckAge
 {
@@ -15,6 +16,13 @@ class CheckAge
      */
     public function handle($request, Closure $next)
     {
+        if (Auth::user()->isAdmin == 1)
+        {
         return $next($request);
+        }
+        else
+        {
+            return redirect('home');
+        }
     }
 }
